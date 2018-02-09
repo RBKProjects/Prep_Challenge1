51ee0bb134f5e03acec6d69443d0cec5ac0fb9b7
Fatima <en.fh90@gmail.com>
2018-02-10 00:08:56 +0200

    init

---
+++ main.js
@@ -0,0 +1,63 @@
+//=============================================== 1
+
+// A restuarnt adds to the original cost of any bill:
+// 8 % tax 
+// 20 % tip
+// write a function called billTotal that takes the bill cost (number) as an input and returns the total bill amount (including the tax and the tips) as an output
+
+function billTotal(subtotal) {
+	// your code is here
+}
+
+//=============================================== 2
+
+//Write a function to greet a person. Function will take name as input and greet the person by saying hello. Return null if input is empty string or null.
+//greet("Niks") === "hello Niks!"
+//greet("") === null; // Return null if input is empty string
+//greet(null) === null; // Return null if input is null
+
+function greet(name) {
+     // TODO: your code here          
+} 
+
+//=============================================== 3
+
+// write a function called addOne that takes an array of numbers as an input,
+// and returns a new array with all the array elements incremented by one as an output
+// solve this question using while loop 
+// addOne( [1,2,3,4] ) ==> [2,3,4,5]
+// addOne( [3,6,9] ) ==> [4,7,10]
+
+function addOne(array){
+	// your code is here
+}
+
+//=============================================== 4
+
+//write a function that returns the last element of a string or an array.
+//last( [1,2,3,4] ) => 4
+//last( "xyz" ) => z
+
+function last(x) {
+	 // TODO: your code here
+}
+
+//write a function that returns the first element of a string or an array.
+//first( [1,2,3,4] ) => 1
+//first( "xyz" ) => x
+
+function first(x) {
+	 // TODO: your code here
+}
+	
+
+//=============================================== 5
+
+//Using recursion, return the sum of all of the positives numbers of an array of numbers.
+// posSum[1,-4,7,12] => 1 + 7 + 12 = 20
+
+function posSum(arr) {
+     // TODO: your code here          
+}
+
+// Good luck :)
